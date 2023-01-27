7f4746fa6b51366770e64630767ab52dafde80e8
mariesaoconnell <mariesa.oconnell@gmail.com>
2023-01-27 16:10:26 -0500

    Add error handling to auth

--- README.md
+++ README.md
@@ -1,16 +1,46 @@
 # VIN FINDER
 
 ## APP DESCRIPTION
-> VIN Finder is a web app that allows users to search VINs for general information about the vehicle and pictures.
+___
+  VIN Finder is a web app that allows users to search VINs for general information about the vehicle and pictures.
 
+___
 ## TECHNOLOGIES USED
-> VIN Finder is a REACTJS Web APP that utilizes:
-> - Bootstrap - for styling
-> - Firebase - for Authentication
-> - React Router - for routing endpoints and protecting paths that require user authorization
-> -
+___
+VIN Finder is a REACTJS Web APP that utilizes:
+ - Bootstrap - for styling
+ - Firebase - for Authentication
+ - React Router - for routing endpoints and protecting paths that require user authorization
 
+___
 ## INSTALLATION
+___
+This application utilizes Google's Programmable Search Engine API, which means it is necessary to have an API KEY and Project ID in order to make this project work.
 
+1. In the root folder, create a '.env' file and the variables should look as follows:
 
+    ```
+    REACT_APP_API_KEY=<YOUR API KEY HERE>
+
+    REACT_APP_PROJECT_ID=<YOUR API KEY HERE>
+    ```
+    Be sure there are no spaces between the variable name, equal sign, and your API Key.
+2. Download the following dependencies.
+    ```
+    npm i react-bootstrap bootstrap dotenv firebase react-chatbot-kit react-router-dom react-switch
+    ```
+3. Now in your console run the command
+    ```
+    npm start
+    ```
+
+___
+## WEB-BASED APP
+___
+Prefer to use the web-based application and not worry about environment variables, downloading dependencies or simply having the space?
+
+You can access VIN Finder by clicking [HERE](https://vinfinder.netlify.app).
+
+___
 ## TUTORIAL
+___
--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,59 +1,93 @@
-import { Form, Button, Container } from 'react-bootstrap';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { Form, Button, Container, Row, Col, Modal } from 'react-bootstrap';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { useState } from 'react';
 import { auth } from './firebase';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import AuthError from '../error-components/AuthError';
 
 import '../../assets/styles/static-styles/AuthForms.css';
+
+// HELPER FUNCTION GRABS THE MESSAGE FROM THE ERROR SO THAT IT CAN BE RENDERED IN THE FORM
+const sliceError = (e) =>{
+	let strError = e.toString();
+	let start = strError.indexOf('(');
+	let end = strError.indexOf(')');
+	let newError = strError.slice(start+1, end)
+	return newError
+}
 
 function SignUp() {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const [error, setError] = useState();
+	const [show, setShow] = useState(false);
 
+	const navigate = useNavigate();
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 	// 'signup' IS THE FUNCTION RUN ON THE FORM'S SUBMISSION. WHEN THE FORM IS SUBMITTED, 'signup' PREVENTS A PAGE REFRESH AND USES FIREBASE'S 'createUserWithEmailAndPassword' TO CREATE A NEW USER WITH THE INFORMATION SUBMITTED IN THE FORM
 	const signup = (e) => {
 		e.preventDefault();
 		createUserWithEmailAndPassword(auth, email, password)
 			.then((userCredential) => {
+				signInWithEmailAndPassword(auth, email, password)
+				localStorage.setItem('userToken', userCredential.user.accessToken);
+				navigate('/')
 				console.log(userCredential);
 			})
-			.catch((error) => {
+			.catch((err) => {
+				setError(sliceError(err))
+				setShow(true);
 				console.log(error);
 			});
 	};
 	return (
-		<Container
-			className='p-5 mx-auto my-5 rounded-5 vertically-align auth-form-styling'>
+		<Container className='p-5 mx-auto my-5 rounded-5 vertically-align auth-form-styling'>
 			<h1
 				className='text-center pt-5 pb-2 hunters'
 				style={{ fontSize: '100px' }}>
-				{' '}
-				Sign Up{' '}
+				Sign Up
 			</h1>
-			<Form onSubmit={signup}>
-				<Form.Group className='my-3'>
-					<Form.Label>Email Address</Form.Label>
-					<Form.Control
-						type='email'
-						placeholder='Example@mail.com'
-						onChange={(e) => setEmail(e.target.value)}
-						required
-					/>
-				</Form.Group>
-				<Form.Group className='my-3'>
-					<Form.Label> Password </Form.Label>
-					<Form.Control
-						type='password'
-						placeholder='Password'
-						onChange={(e) => setPassword(e.target.value)}
-						required
-					/>
-				</Form.Group>
-				<Container className='d-flex pb-5 justify-content-between'>
-					<Link to='/login'>I'm already a member!</Link>
-					<Button type='submit'>Sign Up</Button>
-				</Container>
-			</Form>
+			<Row>
+				<Form onSubmit={signup}>
+					<Form.Group className='my-3'>
+						<Form.Label>Email Address</Form.Label>
+						<Form.Control
+							type='email'
+							placeholder='Example@mail.com'
+							onChange={(e) => setEmail(e.target.value)}
+							required
+						/>
+					</Form.Group>
+					<Form.Group className='my-3'>
+						<Form.Label> Password </Form.Label>
+						<Form.Control
+							type='password'
+							placeholder='Password'
+							onChange={(e) => setPassword(e.target.value)}
+							required
+						/>
+					</Form.Group>
+					<Container>
+						<Row className='d-flex pb-5 justify-content-between'>
+							<Col>
+								<Link to='/login'>I'm already a member!</Link>
+							</Col>
+							<Col sm={2}>
+								<Button type='submit'>Sign Up</Button>
+							</Col>
+						</Row>
+						<Row>
+							{error ? (
+								<AuthError
+									show={show}
+									handleClose={handleClose} error = {error} />
+							) : null}
+						</Row>
+					</Container>
+				</Form>
+			</Row>
 		</Container>
 	);
 }
